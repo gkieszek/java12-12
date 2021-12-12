0058648e976f8afedc4b60094f61f55253c1ddb4
Gregor <i.love.plik@gmail.com>
2021-12-12 15:58:12 +0100

    add featch and template html

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(res => res.json())
 .then(res => {
-    console.log(res)
+    const app = document.getElementById('app')
+    res.forEach(element => {
+        const paragraf = document.createElement('p')
+        paragraf.innerHTML = "<button>" + element.title + "</button>" 
+        // niebezpieczne uzywanie jest innerHTML, powinnien byc innerTekst
+        app.appendChild(paragraf)
+    });
 })
+
